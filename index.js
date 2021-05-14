683cd19e428fea90caef8af856928aa7286fc87c
JoKing <joebryant614@gmail.com>
2021-05-13 21:31:30 -0700

    we know what we gonna do

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
+const inquirer = require("inquirer");
+const Employee = require("./lib/Employee");
 
+function buildPage() {
+
+    inquirer
+        .prompt([{
+            type: 'text',
+            name: 'name',
+            message: 'What is you Employees name?',
+        },
+        {
+            type: 'text',
+            name: 'id',
+            message: 'What is your Employee id?',
+        },
+        {
+            type: 'text',
+            name: 'email',
+            message: 'What is your employee email?',
+        }]).then(answers => {
+            const {name,id,email} = answers;
+            const employee = new Employee(name,id,email);
+
+            printIt(employee);
+        })
+}
+
+
+function printIt(employee) {
+
+    console.log(employee)
+}
+
+buildPage()
