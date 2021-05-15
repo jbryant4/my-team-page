6341f6b2e0f383a80e09154360ae955409136372
JoKing <joebryant614@gmail.com>
2021-05-14 22:48:17 -0700

    engineer tests passed

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,37 +1,44 @@
 const Engineer = require('../lib/Engineer');
 
-test('creates an employee object', () => {
-    const employee = new Employee('joe', 95201, 'email@google.com')
+test('creates an engineer object', () => {
+    const engineer = new Engineer('joe', 95201, 'email@google.com','gitId')
     
-    expect(typeof employee.name).toBe('string')
-    expect(typeof employee.id).toBe('number');
-    expect(typeof employee.email).toBe('string');
+    expect(typeof engineer.name).toBe('string')
+    expect(typeof engineer.id).toBe('number');
+    expect(typeof engineer.email).toBe('string');
+    expect(typeof engineer.githubId).toBe('string')
+
     
 });
 
-test('get employee name',() => {
-    const employee = new Employee('joe', '95201', 'email@google.com')
+test('get engineer name',() => {
+    const engineer = new Engineer('joe', 95201, 'email@google.com','gitId')
 
-    expect(employee.getName()).toEqual(expect.stringContaining(employee.name.toString()));
+    expect(engineer.getName()).toEqual(expect.stringContaining(engineer.name.toString()));
 });
 
-test('get employee Id',() => {
-    const employee = new Employee('joe', '95201', 'email@google.com')
+test('get engineer Id',() => {
+    const engineer = new Engineer('joe', 95201, 'email@google.com','gitId')
 
-    expect(employee.getId()).toEqual(expect.stringContaining(employee.id.toString()));
+    expect(engineer.getId()).toEqual(engineer.id);
 });
 
-test('get employee Email',() => {
-    const employee = new Employee('joe', '95201', 'email@google.com')
+test('get engineer Email',() => {
+    const engineer = new Engineer('joe', 95201, 'email@google.com','gitId')
 
-    expect(employee.getEmail()).toEqual(expect.stringContaining(employee.email.toString()));
+    expect(engineer.getEmail()).toEqual(expect.stringContaining(engineer.email.toString()));
 });
 
+test('get engineer github Id',() => {
+    const engineer = new Engineer('joe', 95201, 'email@google.com','gitId')
 
-test('get employee Role',() => {
-    const employee = new Employee('joe', '95201', 'email@google.com')
+    expect(engineer.getGithubId()).toEqual(expect.stringContaining(engineer.githubId.toString()));
+});
 
-    expect(employee.getRole()).toBe('Employee');
+test('get engineer Role',() => {
+    const engineer = new Engineer('joe', '95201', 'email@google.com','gitId')
+
+    expect(engineer.getRole()).toBe('Engineer');
 });
 
 module.exports = Engineer;
